bdb76ae4dccb3f8c5860fdbaa61c427d8416d061
Franz Wimmer <franz.wimmer@qaware.de>
2021-06-12 15:01:26 +0200

    Display error icon when loading exits with an error

--- src/main/resources/META-INF/resources/js/application.js
+++ src/main/resources/META-INF/resources/js/application.js
@@ -1,6 +1,6 @@
 const serviceUrl = '/coviddata';
 
-$('.spinner').html('<div class="spinner-border spinner-border-sm" role="status"><span class="visually-hidden">Loading...</span></div>');
+$('.spinner').html('<div class="spinner-border spinner-border-sm" role="status"><span class="visually-hidden">Lade Covid-Statistiken...</span></div>');
 
 $.ajax({
     url: serviceUrl,
@@ -20,5 +20,9 @@ $.ajax({
         $('#secondDosePercentage').text(secondDosePercentage);
         $('#deltaCases').text(result.statisticsData.deltaCases);
         $('#weekIncidence').text(weekIncidence);
+    },
+
+    error: function(result) {
+        $('.spinner').html('<i class="bi bi-exclamation-triangle"><span class="visually-hidden">Beim Abruf von Covid-Statistiken ist ein Fehler aufgetreten.</span></i>');
     }
 });
